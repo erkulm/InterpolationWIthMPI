409d94c778ea748e3cc4e214e45a0690e7d9292e
mahmut erkul <mahmuterkul1907@gmail.com>
2016-04-08 01:06:50 +0300

    Initial Commit

---
+++ InterpolationWithMPI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InterpolationWithMPI
+//
+//  Created by Mahmut Erkul on 08/04/16.
+//  Copyright © 2016 Mahmut Erkul. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
